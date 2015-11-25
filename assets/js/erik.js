fc20c176139d6ff225a0788c62cedb3f826fca18
Scott Dafforn <sdafforn@users.noreply.github.com>
2015-11-24 21:15:45 -0500

    Update erik.js

--- assets/js/erik.js
+++ assets/js/erik.js
@@ -1,17 +1,27 @@
-var jsonBaseURL = "http://localhost:8000/assets/content/medicaldata.json";
+var jsonBaseURL = "http://liberationroasting.com/assets/";
 
-var jsonp = function () {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = jsonBaseURL;
-    document.getElementsByTagName("head")[0].appendChild(script);
-}();
+function jsonp(symptom) {
+    // ensure we gracefully handle no symptom
+    symptom = symptom || '';
+    if (symptom !== ''){
+      // inject a script tag requesting symptom json data
+      // ...assumes files are named by symptom
+      var js = document.createElement("script");
+      js.type = "text/javascript";
+      js.src = jsonBaseURL + symptom;
+      document.getElementsByTagName("head")[0].appendChild(js);
+    }
+};
 
 function injectMedicalCondition(conditions) {
     console.log(conditions);
     var index;
+    // iterate all data in the list returned
+    // from the jsonp call
     for (index = 0; index < conditions.length; index++) {
       var condition = conditions[index];
+      // inject a script tag in the head
+      // containing the pertinent json data
       var js = document.createElement('script');
       js.type = 'application/ld+json';
       js.text = JSON.stringify(condition);
@@ -21,3 +31,11 @@ function injectMedicalCondition(conditions) {
       header.innerHTML = ('<h1>' + condition.name + ' injected successfully.</h1>');
     }
 }
+
+function detectSymptom() {
+  // inspect the page to understand
+  // what symptom we need to request
+  var div = document.getElementById('erik');
+  var symptom = div.className;
+  jsonp(symptom);
+}();
